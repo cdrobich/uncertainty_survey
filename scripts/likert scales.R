dcfe4cb30427b4648b91f6e6a08aa560a85c5d72
Courtney Robichaud <44408770+cdrobich@users.noreply.github.com>
2023-06-09 15:39:59 -0400

    Question 1

---
+++ scripts/likert scales.R
@@ -0,0 +1,64 @@
+library(likert)
+library(tidyverse)
+library(stringr)
+library(viridis)
+
+# data with column names as codes
+likert_data <- read.csv("data/uncertainty_data_codes.csv")
+
+
+colnames(likert_data)
+unique(likert_data$often_stakeholders)
+
+### Translate
+
+likert_data$Category <- factor(likert_data$Category,
+                               levels = c("Strongly agree",
+                                          "Somewhat agree",
+                                          "Neither agree nor disagree",
+                                          "Somewhat disagree",
+                                          "Strongly disagree"))
+
+
+
+
+######### Likert Uncertainty Often ########
+
+
+likert_often <- likert_data %>% 
+            select(often_environment:often_funding)
+         
+unique(likert_often$often_evidence)
+
+
+
+
+likert_data %<>% 
+            group_by(Question) %>% 
+            mutate(Sum = sum(Count)) %>% 
+            group_by(Category) %>% 
+            mutate(percent = round(100*Count/Sum, 2))
+
+
+unique(likert_data$Category)
+
+likert_data$Category <- factor(likert_data$Category,
+                               levels = c("Strongly agree",
+                                          "Somewhat agree",
+                                          "Neither agree nor disagree",
+                                          "Somewhat disagree",
+                                          "Strongly disagree"))
+
+myColors <- c("#0571b0","#92c5de","#f7f7f7","#f4a582","#ca0020", 'black')#actual plot creation
+
+ggplot(data = likert_data, aes(x = Question , y = percent, fill = Category)) +
+            geom_bar(stat="identity", width = 0.7) +
+            scale_fill_manual(values = myColors)+
+            theme_classic() +
+            coord_flip() +
+            ylab("Percentage (%)") +
+            xlab(" ") +
+            scale_x_discrete(labels = function(x) str_wrap(x, width = 30)) +
+            theme(axis.text=element_text(size=14),
+                  axis.title=element_text(size=14),
+                  legend.text = element_text(size=12))
--- scripts/question1_pivot.R
+++ scripts/question1_pivot.R
@@ -1,7 +1,171 @@
 library(tidyverse)
+library(fmsb)
+library(forcats)
 
-q1 <- read.csv("data/question1.csv")
+q1 <- read.csv("data/question1_reduced.csv")
+q1p <- read.csv("data/question1_reduced_proportion.csv")
 colnames(q1)
+str(q1) 
+
+### Radar Chart ####
+
+q1_long <- q1 %>% 
+            select(CODE:RANK1)
+
+q1_long <- as.data.frame(q1_long)
+
+dim(q1_long)
+
+q1_rank1_wide <- pivot_wider(q1_long,
+            names_from = CODE,
+            values_from = RANK1)
+
+q1_rank1_wide <- rbind(rep(70,8), rep(0,8), q1_rank1_wide)
+
+
+radarchart(q1_rank1_wide)
 
 
 
+#### Horizontal Lollipop #####
+q1 %>% mutate(CODE = fct_reorder(CODE, RANK1, .desc = FALSE)) %>%
+            ggplot(aes(x = CODE, y = RANK1)) +
+            geom_segment(aes(x = CODE, xend = CODE, y = 0, yend = RANK1),
+                         color="#775C99",
+                         lwd = 2) +
+            geom_point( color="#614B81", size = 6, alpha = 1) +
+            theme_light() +
+            coord_flip() +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 13)) +
+            ylab("Count") +
+            xlab(" ") +
+            ggtitle("1st Ranked")
+
+
+q1 %>% mutate(CODE = fct_reorder(CODE, RANK2prop, .desc = FALSE)) %>%
+            ggplot(aes(x = CODE, y = RANK2prop)) +
+            geom_segment(aes(x = CODE, xend = CODE, y = 0, yend = RANK2prop),
+                         color="#775C99",
+                         lwd = 2) +
+            geom_point( color="#614B81", size = 6, alpha = 1) +
+            theme_light() +
+            coord_flip() +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 13)) +
+            ylab("Count") +
+            xlab(" ") +
+            ggtitle("2nd Ranked")
+
+
+q1 %>% mutate(CODE = fct_reorder(CODE, RANK3prop, .desc = FALSE)) %>%
+            ggplot(aes(x = CODE, y = RANK3prop)) +
+            geom_segment(aes(x = CODE, xend = CODE, y = 0, yend = RANK3prop),
+                         color="#775C99",
+                         lwd = 2) +
+            geom_point( color="#614B81", size = 6, alpha = 1) +
+            theme_light() +
+            coord_flip() +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 13)) +
+            ylab("Count") +
+            xlab(" ") +
+            ggtitle("3rd Ranked")
+
+q1 %>% mutate(CODE = fct_reorder(CODE, RANK4prop, .desc = FALSE)) %>%
+            ggplot(aes(x = CODE, y = RANK4prop)) +
+            geom_segment(aes(x = CODE, xend = CODE, y = 0, yend = RANK4prop),
+                         color="#775C99",
+                         lwd = 2) +
+            geom_point( color="#614B81", size = 6, alpha = 1) +
+            theme_light() +
+            coord_flip() +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 13)) +
+            ylab("Count") +
+            xlab(" ") +
+            ggtitle("4th Ranked")
+
+q1 %>% mutate(CODE = fct_reorder(CODE, RANK5prop, .desc = FALSE)) %>%
+            ggplot(aes(x = CODE, y = RANK5prop)) +
+            geom_segment(aes(x = CODE, xend = CODE, y = 0, yend = RANK5prop),
+                         color="#775C99",
+                         lwd = 2) +
+            geom_point( color="#614B81", size = 6, alpha = 1) +
+            theme_light() +
+            coord_flip() +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 13)) +
+            ylab("Count") +
+            xlab(" ") +
+            ggtitle("5th Ranked")
+
+
+######## Stacked Bar Chart ########
+
+# create a dataset
+
+q1_long <- q1 %>% select(CODE,RANK1:RANK5) %>%
+            pivot_longer(RANK1:RANK5,
+                    names_to = "rank",
+                    values_to = "count")
+
+q1p_long$CODE <- as.factor(q1p_long$CODE)
+
+
+q1_long$rank <- factor(q1_long$rank, levels = c('RANK5',
+                                                'RANK4',
+                                                'RANK3',
+                                                'RANK2',
+                                                'RANK1'))
+
+
+
+unique(q1p_long$CODE)
+
+
+# Stacked
+
+colours = c("RANK1" = "#264653",
+            "RANK2" = "#2A9D8F",
+            "RANK3" = "#E9C46A",
+            "RANK4" = "#F4A261",
+            "RANK5" = "#e76F51")
+
+
+
+q1_bar <- q1_long %>%
+            ggplot(aes(fill = rank, y = count, label = count,
+                       x = factor(CODE,
+                                  level = c('OTHER',
+                                            'NATURE',
+                                            'CLIMATE CHANGE',
+                                            'LEGISLATION',
+                                            'PREDICTION',
+                                            'SPECIES',
+                                            'SUPPORT',
+                                            'RESOURCES',
+                                            'POLITICAL WILL',
+                                            'COLLABORATION',
+                                            'FUNDING',
+                                            'INFORMATION'
+                                            )))) + 
+            geom_bar(position = "stack", stat = "identity") +
+            coord_flip() +
+            xlab(" ") +
+            ylab("Count") +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 13),
+                  legend.position = c(0.7, 0.3),
+                  legend.key.size = unit(1.8, 'cm'),
+                  legend.text = element_text(size = 12),
+                  legend.direction = "horizontal",
+                  legend.title = element_blank()) +
+            geom_text(size = 5, alpha = 1, colour = "white",
+                      position = position_stack(vjust = 0.55)) +
+            guides(fill = guide_legend(reverse = TRUE,
+                                       label.position = "bottom")) +
+            scale_fill_manual(values = colours)
+q1_bar 
+
+ggsave("output/q1_bar.jpeg")
