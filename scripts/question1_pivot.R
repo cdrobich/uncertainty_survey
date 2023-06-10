1f3d0ce646402d5893f3c3de82a99634cea8bae9
Courtney Robichaud <44408770+cdrobich@users.noreply.github.com>
2023-06-09 21:35:26 -0400

    Updated figures

--- scripts/question1_pivot.R
+++ scripts/question1_pivot.R
@@ -155,7 +155,8 @@ q1_bar <- q1_long %>%
             xlab(" ") +
             ylab("Count") +
             theme_minimal() +
-            theme(axis.text = element_text(size = 13),
+            theme(axis.text = element_text(size = 16),
+                  axis.title = element_text(size = 16),
                   legend.position = c(0.7, 0.3),
                   legend.key.size = unit(1.8, 'cm'),
                   legend.text = element_text(size = 12),
@@ -169,3 +170,197 @@ q1_bar <- q1_long %>%
 q1_bar 
 
 ggsave("output/q1_bar.jpeg")
+
+
+########## Specific Topics ######
+
+breakdown <- read.csv("data/question1_summary.csv")
+
+breakdown <- breakdown %>% select(CODE:RANK5) %>%
+            pivot_longer(RANK1:RANK5,
+                         names_to = "rank",
+                         values_to = "count")
+
+
+breakdown$rank <- factor(breakdown$rank, levels = c('RANK5',
+                                                'RANK4',
+                                                'RANK3',
+                                                'RANK2',
+                                                'RANK1'))
+
+
+
+### Resources 
+
+rs <- c("RESOURCES", "CAPACITY", "PERSONNEL", "EXPERTISE")
+
+resources <- breakdown %>%
+            filter(CODE %in% rs)
+
+resources %>% ggplot(aes(fill = rank, y = count, label = count,
+                        x = factor(CODE,
+                                   level = c('EXPERTISE',
+                                             'PERSONNEL',
+                                             'RESOURCES',
+                                             'CAPACITY'
+                                   )))) + 
+            geom_bar(position = "stack", stat = "identity") +
+            coord_flip() +
+            xlab(" ") +
+            ylab("Count") +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 20),
+                  axis.title = element_text(size = 20),
+                  #legend.position = "none",
+                  legend.key.size = unit(1, 'cm'),
+                  legend.text = element_text(size = 12),
+                  #legend.direction = "horizontal",
+                  legend.title = element_blank()) +
+            geom_text(size = 10, alpha = 1, colour = "white",
+                      position = position_stack(vjust = 0.55)) +
+            guides(fill = guide_legend(reverse = TRUE,
+                                       label.position = "bottom")) +
+            scale_fill_manual(values = colours)
+
+##### funding
+
+fd <- c("FUNDING", "FUND_AMOUNT", "FUND_LENGTH", 
+        "FUND_AVAIL")
+
+funding <- breakdown %>%
+            filter(CODE %in% fd)
+
+
+funding %>% ggplot(aes(fill = rank, y = count, label = count,
+                      x = factor(CODE,
+                                 level = c('FUND_AMOUNT',
+                                           'FUND_LENGTH',
+                                           'FUND_AVAIL',
+                                           'FUNDING'
+                                 ))))+ 
+            geom_bar(position = "stack", stat = "identity") +
+            coord_flip() +
+            xlab(" ") +
+            ylab("Count") +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 20),
+                  axis.title = element_text(size = 20),
+                  #legend.position = "none",
+                  legend.key.size = unit(1, 'cm'),
+                  legend.text = element_text(size = 12),
+                  #legend.direction = "horizontal",
+                  legend.title = element_blank()) +
+            geom_text(size = 7, alpha = 1, colour = "white",
+                      position = position_stack(vjust = 0.55)) +
+            guides(fill = guide_legend(reverse = TRUE,
+                                       label.position = "bottom")) +
+            scale_fill_manual(values = colours)
+
+##### Information
+
+info <- c("CONSENSUS", "DATA", "LONGTERM", "SITE",
+          "IMPACT", "METHODS", "CUMULATIVE", "SUCCESS_CRITERIA",
+          "SPECIES")
+
+inform <- breakdown %>%
+            filter(CODE %in% info)
+
+
+inform %>% ggplot(aes(fill = rank, y = count, label = count,
+                     x = factor(CODE,
+                                level = c('SITE',
+                                          'CONSENSUS',
+                                          'SUCCESS_CRITERIA',
+                                          'CUMULATIVE',
+                                          'IMPACT',
+                                          'LONGTERM',
+                                          'METHODS',
+                                          'SPECIES',
+                                          'DATA'
+                                )))) + 
+            geom_bar(position = "stack", stat = "identity") +
+            coord_flip() +
+            xlab(" ") +
+            ylab("Count") +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 20),
+                  axis.title = element_text(size = 20),
+                  #legend.position = "bottom",
+                  legend.key.size = unit(1, 'cm'),
+                  legend.text = element_text(size = 12),
+                  #legend.direction = "horizontal",
+                  legend.title = element_blank()) +
+            geom_text(size = 6, alpha = 1, colour = "white",
+                      position = position_stack(vjust = 0.55)) +
+            guides(fill = guide_legend(reverse = TRUE,
+                                       label.position = "bottom")) +
+            scale_fill_manual(values = colours)
+
+
+
+##### Collaboration
+
+
+CL <- c("COMM_COLLAB", "RESPONSIBILITIES", "COMPLEXITY", "COSTS_TRADEOFF")
+
+collab <- breakdown %>%
+            filter(CODE %in% CL)
+
+
+collab %>% ggplot(aes(fill = rank, y = count, label = count,
+                      x = factor(CODE,
+                                 level = c('COMPLEXITY',
+                                           'RESPONSIBILITIES',
+                                           'COSTS_TRADEOFF',
+                                           'COMM_COLLAB'
+                                 )))) + 
+            geom_bar(position = "stack", stat = "identity") +
+            coord_flip() +
+            xlab(" ") +
+            ylab("Count") +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 20),
+                  axis.title = element_text(size = 20),
+                  #legend.position = "bottom",
+                  legend.key.size = unit(1, 'cm'),
+                  legend.text = element_text(size = 12),
+                  #legend.direction = "horizontal",
+                  legend.title = element_blank()) +
+            geom_text(size = 6, alpha = 1, colour = "white",
+                      position = position_stack(vjust = 0.55)) +
+            guides(fill = guide_legend(reverse = TRUE,
+                                       label.position = "bottom")) +
+            scale_fill_manual(values = colours)
+
+
+
+
+
+
+## Political Will
+
+PW <- breakdown %>%
+            filter(CODE %in% c('POLITICAL_WILL', 'LEGISLATION'))
+
+PW %>% ggplot(aes(fill = rank, y = count, label = count,
+                      x = CODE)) + 
+            geom_bar(position = "stack", stat = "identity") +
+            coord_flip() +
+            xlab(" ") +
+            ylab("Count") +
+            theme_minimal() +
+            theme(axis.text = element_text(size = 20),
+                  axis.title = element_text(size = 20),
+                  #legend.position = "bottom",
+                  legend.key.size = unit(1, 'cm'),
+                  legend.text = element_text(size = 12),
+                  #legend.direction = "horizontal",
+                  legend.title = element_blank()) +
+            geom_text(size = 6, alpha = 1, colour = "white",
+                      position = position_stack(vjust = 0.55)) +
+            guides(fill = guide_legend(reverse = TRUE,
+                                       label.position = "bottom")) +
+            scale_fill_manual(values = colours)
+
+
+
